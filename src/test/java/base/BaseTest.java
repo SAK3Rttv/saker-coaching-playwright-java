74f110399c641dae435bae43f697c2300c7c377e
Abdullah Muhaisen <engabdullahsak@gmail.com>
2026-05-21 22:31:27 +0300

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -12,9 +12,6 @@ import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.options.AriaRole;
-
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 import config.ConfigReader;
 import listeners.TestListener;
